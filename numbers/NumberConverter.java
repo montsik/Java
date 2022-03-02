6f2441fe815883ea41d8294095931829c3f62170
montsik <90908331+montsik@users.noreply.github.com>
2022-03-02 19:08:15 +0200

    Add files via upload

---
+++ numbers/NumberConverter.java
@@ -0,0 +1,12 @@
+package exceptions.numbers;
+
+public class NumberConverter {
+
+    public NumberConverter(String lang) {
+        throw new RuntimeException("not implemented yet");
+    }
+
+    public String numberInWords(Integer number) {
+        throw new RuntimeException("not implemented yet");
+    }
+}
